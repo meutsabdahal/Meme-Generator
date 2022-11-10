4d60f54e7fa49fd13a2b727418dcfaa4f93ff589
Utsab Dahal <70103872+meutsabdahal@users.noreply.github.com>
2022-11-10 08:38:41 +0545

    Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+function Header() {
+    return(
+        <header>
+            {/* adding images and tiltle to the header componrnt */}
+        <img src="../img/troll-face.png" className="header-image" />
+        <h2 className="header-title">Meme Generator</h2>
+        </header>
+
+    )
+}
+
+export default Header
