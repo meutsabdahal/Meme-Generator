8c3039dda73b2d2ae1fc664b26a1c5c175ee4d05
Utsab Dahal <70103872+meutsabdahal@users.noreply.github.com>
2022-11-12 16:43:42 +0545

    Assigning images to the meme generator

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,31 @@
+import React from 'react';
+
+import memesData from '../memesData.js'
+
 
 function Meme() {
+
+    const [memeImage, setMemeImage] = React.useState('')
+
+    // function to randomly generate meme images
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const random = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[random].url)
+
+    }
+
     return(
         <main>
             {/* form component */}
-            <form className="form">
+            <div className="form">
                 <input type = 'text' placeholder= 'Top Text' className = 'input-form' />
                 <input type = 'text' placeholder= 'Botton Text' className = 'input-form' />
-                <button className = 'button-form'>Get a New Meme Image</button>
-            </form>
+
+                <button className = 'button-form' onClick = {getMemeImage}>Get a New Meme Image</button>
+            </div>
+
+        <center><img src = {memeImage} className = 'meme-image'/></center>
         </main>
     )
 }
